f7efc953687071b0601677ca919d143a891b408f
Tercio de Melo <github@tercio.com.br>
2021-01-17 20:01:04 -0300

    Using leiningen instead of pure deps.edn

---
+++ src/main.clj
@@ -0,0 +1,11 @@
+(ns main
+  (:gen-class)
+  (:use [hiccup.core]))
+
+(defn -main [& args]
+  (println (html [:head
+                  :link {:rel "stylesheet" :href "index.css"}]
+                 [:body
+                  [:p "New website is comming along"]]
+                 [:footer
+                  [:span "2020-2021"]])))
